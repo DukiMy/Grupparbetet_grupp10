e70838c1923878af3eef9d2536d8ca2e4e0229cb
yourfoxon <90153524+yourfoxon@users.noreply.github.com>
2022-03-17 20:11:54 +0100

    Added connection between user and interestreg

---
+++ Migrations/20220316210554_added_interestregistration.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HomeFinder.Migrations
+{
+    public partial class added_interestregistration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserId",
+                table: "InterestRegistration",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InterestRegistration_ApplicationUserId",
+                table: "InterestRegistration",
+                column: "ApplicationUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_InterestRegistration_AspNetUsers_ApplicationUserId",
+                table: "InterestRegistration",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_InterestRegistration_AspNetUsers_ApplicationUserId",
+                table: "InterestRegistration");
+
+            migrationBuilder.DropIndex(
+                name: "IX_InterestRegistration_ApplicationUserId",
+                table: "InterestRegistration");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserId",
+                table: "InterestRegistration");
+        }
+    }
+}
