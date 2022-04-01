f030f1d5c1a8e9aaddc172a5f2c7d600fc2ef419
yourfoxon <90153524+yourfoxon@users.noreply.github.com>
2022-04-01 08:52:51 +0200

    Removed viewmodelconverter class

--- Repository/ItemRepository.cs
+++ Repository/ItemRepository.cs
@@ -41,8 +41,7 @@ namespace HomeFinder.Repository
 
         public int AddNewItemFromModel(ItemViewModel model, ApplicationUser broker)
         {
-            var itemType = GetItemType(model.ItemType);
-            Item item = ViewModelToModelConverter.CreateItem(model, itemType, broker); 
+            Item item = CreateItemFromViewModel(model, broker);
             _context.Items.Add(item);
             _context.SaveChanges();
 
@@ -51,11 +50,8 @@ namespace HomeFinder.Repository
 
         public async Task Update(ItemViewModel model, ApplicationUser broker)
         {
-            var itemType = GetItemType(model.ItemType);
-            Item item = ViewModelToModelConverter.CreateItem(model, itemType, broker);
-
+            Item item = CreateItemFromViewModel(model, broker);
             _context.Update(item);
-            
             await _context.SaveChangesAsync();
         }
 
@@ -77,9 +73,47 @@ namespace HomeFinder.Repository
             return _context.Items.Any(e => e.Id == id);
         }
 
-        private ItemType GetItemType(string itemType)
+        public Item CreateItemFromViewModel(ItemViewModel model, ApplicationUser broker)
         {
-            return _context.ItemTypes.FirstOrDefault(it => it.Name == itemType);
+            var itemType = _context.ItemTypes.FirstOrDefault(it => it.Name == model.ItemType);
+
+            var item = new Item()
+            {
+                ItemType = itemType,
+                Price = model.Price,
+                FormOfLease = model.FormOfLease,
+
+                Address = model.Address,
+                City = model.City,
+                ZipCode = model.ZipCode,
+
+                Description = model.Description,
+
+                NrOfRoom = model.NrOfRoom,
+                LivingArea = model.LivingArea,
+                GrossFloorArea = model.GrossFloorArea,
+                PlotArea = model.PlotArea,
+
+                ConstructionYear = model.ConstructionYear,
+                ListingDate = model.ListingDate,
+
+                MainImageUrl = model.MainImageUrl,
+
+                Broker = broker
+            };
+
+            item.itemGallery = new List<Image>();
+
+            foreach (var file in model.Images)
+            {
+                item.itemGallery.Add(new Image()
+                {
+                    Title = file.Title,
+                    URL = file.URL
+                });
+            }
+
+            return item;
         }
     }
 }
